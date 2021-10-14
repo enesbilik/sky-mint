bff1e54fd98fbd2c85d4d58e6312c8672a6089eb
Samet Burhan <samet26burhan2@gmail.com>
2021-10-14 19:20:16 +0300

    color update

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -4,4 +4,4 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff14146F);
-const softwhiteColor = Color(0xffD8D8FF);
+const softwhiteColor = Color(0xffEBEBFF);
