c70fed6abc098d0d23b09320651d72984172de15
Enes Bilik <enesbilik8@gmail.com>
2021-10-15 20:17:40 +0300

    postlar güncellendi

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -2,9 +2,15 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var listOfPostName = ["Nasa", "Orta Bahçe", "Taş Bina"];
   @override
   Widget build(BuildContext context) {
     Size deviceSize = MediaQuery.of(context).size;
@@ -18,85 +24,91 @@ class HomePage extends StatelessWidget {
   }
 
   Widget buildBody() {
-    return Expanded(
-      child: RefreshIndicator(
-        onRefresh: () async {
-          final random = Random();
-          await Future.delayed(Duration(seconds: random.nextInt(2)));
+    return RefreshIndicator(
+      onRefresh: () async {
+        final random = Random();
+        await Future.delayed(Duration(seconds: random.nextInt(2)));
+      },
+      child: ListView.builder(
+        physics: const AlwaysScrollableScrollPhysics(),
+        itemCount: 7,
+        itemBuilder: (BuildContext context, int index) {
+          int _index = index % 3;
+
+          return buildPostItem(_index);
         },
-        child: ListView.builder(
-          physics: const AlwaysScrollableScrollPhysics(),
-          itemCount: 10,
-          itemBuilder: (BuildContext context, int index) {
-            return Column(
-              // ignore: prefer_const_literals_to_create_immutables
-              children: [
-                ListTile(
-                  title: Text(
-                    "Nasa",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: Text(
-                    "Moon",
-                  ),
-                  leading: CircleAvatar(),
-                  trailing: IconButton(
-                    padding: EdgeInsets.zero,
-                    constraints: BoxConstraints(),
-                    icon: Icon(Icons.more_vert),
-                    onPressed: () {},
-                  ),
-                ),
-                Image.asset(
-                  "assets/images/post_image.jpeg",
-                  width: double.infinity,
-                  fit: BoxFit.contain,
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 10.0, vertical: 4),
-                  child: Row(
-                    children: [
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.favorite_border,
-                          size: 30,
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.messenger_outline,
-                          size: 28,
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.send,
-                          size: 28,
-                        ),
-                      ),
-                      Spacer(),
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.arrow_drop_down_circle_outlined,
-                          size: 32,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Divider(
-                  thickness: 0.3,
-                ),
-              ],
-            );
-          },
-        ),
       ),
+    );
+  }
+
+  Widget buildPostItem(int index) {
+    String _imageUrl = "assets/images/post_images/post_image$index.jpeg";
+    return Column(
+      // ignore: prefer_const_literals_to_create_immutables
+      children: [
+        ListTile(
+          title: Text(
+            listOfPostName[index],
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text(
+            listOfPostName[index],
+          ),
+          leading: CircleAvatar(
+            backgroundImage: AssetImage(_imageUrl),
+          ),
+          trailing: IconButton(
+            padding: EdgeInsets.zero,
+            constraints: BoxConstraints(),
+            icon: Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ),
+        Image.asset(
+          _imageUrl,
+          width: double.infinity,
+          fit: BoxFit.contain,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 4),
+          child: Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  index % 2 == 0 ? Icons.star_border : Icons.star_outlined,
+                  size: 32,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.messenger_outline,
+                  size: 28,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.send,
+                  size: 28,
+                ),
+              ),
+              Spacer(),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.arrow_drop_down_circle_outlined,
+                  size: 32,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Divider(
+          thickness: 0.3,
+        ),
+      ],
     );
   }
 }
