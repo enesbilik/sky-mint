3e384e856c8ad17de0c5918c399c5d472db24192
Umut Güzel <umutguzel.ug@gmail.com>
2021-10-21 01:41:06 +0300

    Revert "food model düzenlendi"
    This reverts commit 73a4cd2ccd4bb9ddc913f5f7f12a4b405c2ac36b.

--- lib/models/food_model.dart
+++ lib/models/food_model.dart
@@ -1,14 +1,37 @@
 class FoodModel {
   String date;
   String day;
-  String lunchFood;
-  String dinnerFood;
+  List<String> lunchFood;
+  List<String> dinnerFood;
 
   FoodModel(this.date, this.day, this.lunchFood, this.dinnerFood);
 }
 
-var tempFoodMenu = FoodModel(
-    "20.10.2021",
-    "Çarşamba",
-    'KAŞARLI DOMATES ÇORBA\nKIYMALI PATATES OTURTMA\nPENNE ARABİATTA\nVEJ. SÜTLÜ PATATES\nSEÇMELİ SALATA-SANDVİÇ BÜFE',
-    'DÜĞÜN ÇORBA\nJULYEN DİLİMLİTAVUK\nPENNE ARABİATTA\nVEJ. SÜTLÜ PATATES\nSEÇMELİ SALATA-SANDVİÇ BÜFE');
+List<FoodModel> listOfFood = [
+  FoodModel("20.10.2021", "Çarşamba", [
+    'KAŞARLI DOMATES ÇORBA',
+    'KIYMALI PATATES OTURTMA',
+    'PENNE ARABİATTA',
+    'VEJ. SÜTLÜ PATATES',
+    'SEÇMELİ SALATA-SANDVİÇ BÜFE'
+  ], [
+    'DÜĞÜN ÇORBA',
+    'JULYEN DİLİMLİTAVUK',
+    'PENNE ARABİATTA',
+    'VEJ. SÜTLÜ PATATES',
+    'SEÇMELİ SALATA-SANDVİÇ BÜFE'
+  ]),
+  FoodModel("21.10.2021", "Perşembe", [
+    'EZOGELİN ÇORBA',
+    'TAVUK DÖNER',
+    'PİRİNÇ PİLAVI',
+    'VEJ.NOHUTLU PATLICAN',
+    'SEÇMELİ SALATA-SANDVİÇ BÜFE'
+  ], [
+    'YAYLA ÇORBA',
+    'ET DÖNER',
+    'PİRİNÇ PİLAVI',
+    'VEJ.NOHUTLU PATLICAN',
+    'SEÇMELİ SALATA-SANDVİÇ BÜFE'
+  ]),
+];
