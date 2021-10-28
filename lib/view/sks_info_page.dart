efa799705cef7509b683b90440b814bf19face5c
Umut Güzel <umutguzel.ug@gmail.com>
2021-10-28 17:29:13 +0300

    ring clocks added

--- lib/view/sks_info_page.dart
+++ lib/view/sks_info_page.dart
@@ -36,54 +36,77 @@ class _InfoPageState extends State<InfoPage> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Container(
-            width: deviceSize.width * 0.85,
-            height: deviceSize.height * 0.35,
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Color(0xff75C2FF).withOpacity(0.5),
-                  spreadRadius: 1,
-                  blurRadius: 7,
-                  offset: Offset(0, 3), // changes position of shadow
+              width: deviceSize.width * 0.85,
+              height: deviceSize.height * 0.35,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xff75C2FF).withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 7,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(30),
+                gradient: const LinearGradient(
+                  colors: foodContainerColors,
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
                 ),
-              ],
-              borderRadius: BorderRadius.circular(30),
-              gradient: const LinearGradient(
-                colors: foodContainerColors,
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
               ),
-            ),
-            child: Stack(children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [Image.asset('assets/pin.png',width: 60,),],)
-              ,
-              Container(
-                margin: EdgeInsets.only(top:20),
-                child: buildFoodText(tempFoodMenu),)
-            ],)
-          ),
+              child: Stack(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Image.asset(
+                        'assets/pin.png',
+                        width: 60,
+                      ),
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: buildFoodText(tempFoodMenu),
+                  )
+                ],
+              )),
           Container(
-            width: deviceSize.width * 0.85,
-            height: deviceSize.height * 0.35,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              boxShadow: [
-                BoxShadow(
-                  color: Color(0xff75C2FF).withOpacity(0.5),
-                  spreadRadius: 1,
-                  blurRadius: 7,
-                  offset: Offset(0, 3), // changes position of shadow
+              width: deviceSize.width * 0.85,
+              height: deviceSize.height * 0.35,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xff75C2FF).withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 7,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+                gradient: const LinearGradient(
+                  colors: foodContainerColors,
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
                 ),
-              ],
-              gradient: const LinearGradient(
-                colors: foodContainerColors,
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
               ),
-            ),
-          ),
+              child: Stack(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Image.asset(
+                        'assets/pin.png',
+                        width: 60,
+                      ),
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: buildRingText(),
+                  )
+                ],
+              )),
         ],
       ),
     );
@@ -162,6 +185,181 @@ class _InfoPageState extends State<InfoPage> {
                           height: 1.5,
                           fontSize: 17,
                           fontFamily: "Poppins"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  buildRingText() {
+    return Padding(
+      padding: const EdgeInsets.all(24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                "Ring Saatleri",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontFamily: "Poppins Bold",
+                ),
+              )
+            ],
+          ),
+          Expanded(
+            child: PageView(
+              controller: controller,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const [
+                    Text(
+                      "Davutpaşa-Cevizlibağ Ring",
+                      style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 0.7,
+                          height: 1.5,
+                          fontSize: 17,
+                          fontFamily: "Poppins Bold"),
+                    ),
+                    Text(
+                      "07.30-10.00 arası 5 dakikada bir\n 10.15-18.00 arası 15 dakikada bir\n18.30-21.00 arası 30 dakikada bir\n",
+                      style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 0.7,
+                          height: 1.5,
+                          fontSize: 14,
+                          fontFamily: "Poppins"),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const [
+                    Text(
+                      "Kampüs İçi Ring",
+                      style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 0.7,
+                          height: 1.5,
+                          fontSize: 17,
+                          fontFamily: "Poppins Bold"),
+                    ),
+                    Text(
+                      "07.30-10.00 arası 5 dakikada bir\n 10.15-18.00 arası 15 dakikada bir",
+                      style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 0.7,
+                          height: 1.5,
+                          fontSize: 13,
+                          fontFamily: "Poppins"),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "Davutpaşa-Yıldız     Yıldız-Davutpaşa",
+                      style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 0.7,
+                          height: 1.5,
+                          fontSize: 13,
+                          fontFamily: "Poppins Bold"),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: const [
+/*
+                            Text(
+                              "Davutpaşa-Yıldız",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  letterSpacing: 0.7,
+                                  height: 1.5,
+                                  fontSize: 15,
+                                  fontFamily: "Poppins Bold"),
+                            ),*/
+                            Text(
+                              "->08.00\n->09.00\n->10.00\n->11.00\n->12.15",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  //letterSpacing: 0.7,
+                                  height: 1.5,
+                                  fontSize: 12,
+                                  fontFamily: "Poppins"),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: const [
+                            /*Text(
+                              "Yıldız-Davutpaşa",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  letterSpacing: 0.7,
+                                  height: 1.5,
+                                  fontSize: 15,
+                                  fontFamily: "Poppins Bold"),
+                            ),*/
+                            Text(
+                              "->13.00\n->14.00\n->15.00\n->16.00",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  //letterSpacing: 0.7,
+                                  height: 1.5,
+                                  fontSize: 12,
+                                  fontFamily: "Poppins"),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: const [
+                            Text(
+                              "->07.30\n->09.00\n->10.00\n->11.00\n->12.15",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  //letterSpacing: 0.7,
+                                  height: 1.5,
+                                  fontSize: 12,
+                                  fontFamily: "Poppins"),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: const [
+                            /*Text(
+                              "Yıldız-Davutpaşa",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  letterSpacing: 0.7,
+                                  height: 1.5,
+                                  fontSize: 15,
+                                  fontFamily: "Poppins Bold"),
+                            ),*/
+                            Text(
+                              "->13.00\n->14.00\n->15.00\n->16.00",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  //letterSpacing: 0.7,
+                                  height: 1.5,
+                                  fontSize: 12,
+                                  fontFamily: "Poppins"),
+                            ),
+                          ],
+                        )
+                      ],
                     ),
                   ],
                 ),
